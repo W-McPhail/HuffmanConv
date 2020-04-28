2550b4c66c7711a3cb06f22b460ccbed7ecf1e14
w-mcphail <william.m.mcphail@gmaill.com>
2020-04-28 10:38:37 -0400

    .java

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,153 +1,221 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
+/*
+ * Copyright 2014, Michael T. Goodrich, Roberto Tamassia, Michael H. Goldwasser
+ *
+ * Developed for use with the book:
+ *
+ *    Data Structures and Algorithms in Java, Sixth Edition
+ *    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
+ *    John Wiley & Sons, 2014
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+/**
+ * A basic singly linked list implementation.
+ *
+ * @author Michael T. Goodrich
+ * @author Roberto Tamassia
+ * @author Michael H. Goldwasser
+ */
 public class SinglyLinkedList<E> implements Cloneable {
-    private SinglyLinkedList.Node<E> head = null;
-    private SinglyLinkedList.Node<E> tail = null;
-    private int size = 0;
+    //---------------- nested Node class ----------------
+    /**
+     * Node of a singly linked list, which stores a reference to its
+     * element and to the subsequent node in the list (or null if this
+     * is the last node).
+     */
+    private static class Node<E> {
 
-    public SinglyLinkedList() {
-    }
+        /** The element stored at this node */
+        private E element;            // reference to the element stored at this node
 
-    public int size() {
-        return this.size;
-    }
+        /** A reference to the subsequent node in the list */
+        private Node<E> next;         // reference to the subsequent node in the list
 
-    public boolean isEmpty() {
-        return this.size == 0;
-    }
-
-    public E first() {
-        return this.isEmpty() ? null : this.head.getElement();
-    }
-
-    public E last() {
-        return this.isEmpty() ? null : this.tail.getElement();
-    }
-
-    public void addFirst(E e) {
-        this.head = new SinglyLinkedList.Node(e, this.head);
-        if (this.size == 0) {
-            this.tail = this.head;
+        /**
+         * Creates a node with the given element and next node.
+         *
+         * @param e  the element to be stored
+         * @param n  reference to a node that should follow the new node
+         */
+        public Node(E e, Node<E> n) {
+            element = e;
+            next = n;
         }
 
-        ++this.size;
+        // Accessor methods
+        /**
+         * Returns the element stored at the node.
+         * @return the element stored at the node
+         */
+        public E getElement() { return element; }
+
+        /**
+         * Returns the node that follows this one (or null if no such node).
+         * @return the following node
+         */
+        public Node<E> getNext() { return next; }
+
+        // Modifier methods
+        /**
+         * Sets the node's next reference to point to Node n.
+         * @param n    the node that should follow this one
+         */
+        public void setNext(Node<E> n) { next = n; }
+    } //----------- end of nested Node class -----------
+
+    // instance variables of the SinglyLinkedList
+    /** The head node of the list */
+    private Node<E> head = null;               // head node of the list (or null if empty)
+
+    /** The last node of the list */
+    private Node<E> tail = null;               // last node of the list (or null if empty)
+
+    /** Number of nodes in the list */
+    private int size = 0;                      // number of nodes in the list
+
+    /** Constructs an initially empty list. */
+    public SinglyLinkedList() { }              // constructs an initially empty list
+
+    // access methods
+    /**
+     * Returns the number of elements in the linked list.
+     * @return number of elements in the linked list
+     */
+    public int size() { return size; }
+
+    /**
+     * Tests whether the linked list is empty.
+     * @return true if the linked list is empty, false otherwise
+     */
+    public boolean isEmpty() { return size == 0; }
+
+    /**
+     * Returns (but does not remove) the first element of the list
+     * @return element at the front of the list (or null if empty)
+     */
+    public E first() {             // returns (but does not remove) the first element
+        if (isEmpty()) return null;
+        return head.getElement();
     }
 
-    public void addLast(E e) {
-        SinglyLinkedList.Node<E> newest = new SinglyLinkedList.Node(e, (SinglyLinkedList.Node)null);
-        if (this.isEmpty()) {
-            this.head = newest;
-        } else {
-            this.tail.setNext(newest);
-        }
-
-        this.tail = newest;
-        ++this.size;
+    /**
+     * Returns (but does not remove) the last element of the list.
+     * @return element at the end of the list (or null if empty)
+     */
+    public E last() {              // returns (but does not remove) the last element
+        if (isEmpty()) return null;
+        return tail.getElement();
     }
 
-    public E removeFirst() {
-        if (this.isEmpty()) {
-            return null;
-        } else {
-            E answer = this.head.getElement();
-            this.head = this.head.getNext();
-            --this.size;
-            if (this.size == 0) {
-                this.tail = null;
-            }
-
-            return answer;
-        }
+    // update methods
+    /**
+     * Adds an element to the front of the list.
+     * @param e  the new element to add
+     */
+    public void addFirst(E e) {                // adds element e to the front of the list
+        head = new Node<>(e, head);              // create and link a new node
+        if (size == 0)
+            tail = head;                           // special case: new node becomes tail also
+        size++;
     }
 
+    /**
+     * Adds an element to the end of the list.
+     * @param e  the new element to add
+     */
+    public void addLast(E e) {                 // adds element e to the end of the list
+        Node<E> newest = new Node<>(e, null);    // node will eventually be the tail
+        if (isEmpty())
+            head = newest;                         // special case: previously empty list
+        else
+            tail.setNext(newest);                  // new node after existing tail
+        tail = newest;                           // new node becomes the tail
+        size++;
+    }
+
+    /**
+     * Removes and returns the first element of the list.
+     * @return the removed element (or null if empty)
+     */
+    public E removeFirst() {                   // removes and returns the first element
+        if (isEmpty()) return null;              // nothing to remove
+        E answer = head.getElement();
+        head = head.getNext();                   // will become null if list had only one node
+        size--;
+        if (size == 0)
+            tail = null;                           // special case as list is now empty
+        return answer;
+    }
+
+    @SuppressWarnings({"unchecked"})
     public boolean equals(Object o) {
-        if (o == null) {
-            return false;
-        } else if (this.getClass() != o.getClass()) {
-            return false;
-        } else {
-            SinglyLinkedList other = (SinglyLinkedList)o;
-            if (this.size != other.size) {
-                return false;
-            } else {
-                SinglyLinkedList.Node walkA = this.head;
-
-                for(SinglyLinkedList.Node walkB = other.head; walkA != null; walkB = walkB.getNext()) {
-                    if (!walkA.getElement().equals(walkB.getElement())) {
-                        return false;
-                    }
-
-                    walkA = walkA.getNext();
-                }
-
-                return true;
-            }
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        SinglyLinkedList other = (SinglyLinkedList) o;   // use nonparameterized type
+        if (size != other.size) return false;
+        Node walkA = head;                               // traverse the primary list
+        Node walkB = other.head;                         // traverse the secondary list
+        while (walkA != null) {
+            if (!walkA.getElement().equals(walkB.getElement())) return false; //mismatch
+            walkA = walkA.getNext();
+            walkB = walkB.getNext();
         }
+        return true;   // if we reach this, everything matched successfully
     }
 
+    @SuppressWarnings({"unchecked"})
     public SinglyLinkedList<E> clone() throws CloneNotSupportedException {
-        SinglyLinkedList<E> other = (SinglyLinkedList)super.clone();
-        if (this.size > 0) {
-            other.head = new SinglyLinkedList.Node(this.head.getElement(), (SinglyLinkedList.Node)null);
-            SinglyLinkedList.Node<E> walk = this.head.getNext();
-
-            for(SinglyLinkedList.Node otherTail = other.head; walk != null; walk = walk.getNext()) {
-                SinglyLinkedList.Node<E> newest = new SinglyLinkedList.Node(walk.getElement(), (SinglyLinkedList.Node)null);
-                otherTail.setNext(newest);
+        // always use inherited Object.clone() to create the initial copy
+        SinglyLinkedList<E> other = (SinglyLinkedList<E>) super.clone(); // safe cast
+        if (size > 0) {                    // we need independent chain of nodes
+            other.head = new Node<>(head.getElement(), null);
+            Node<E> walk = head.getNext();      // walk through remainder of original list
+            Node<E> otherTail = other.head;     // remember most recently created node
+            while (walk != null) {              // make a new node storing same element
+                Node<E> newest = new Node<>(walk.getElement(), null);
+                otherTail.setNext(newest);     // link previous node to this one
                 otherTail = newest;
+                walk = walk.getNext();
             }
         }
-
         return other;
     }
 
     public int hashCode() {
         int h = 0;
-
-        for(SinglyLinkedList.Node walk = this.head; walk != null; walk = walk.getNext()) {
-            h ^= walk.getElement().hashCode();
-            h = h << 5 | h >>> 27;
+        for (Node walk=head; walk != null; walk = walk.getNext()) {
+            h ^= walk.getElement().hashCode();      // bitwise exclusive-or with element's code
+            h = (h << 5) | (h >>> 27);              // 5-bit cyclic shift of composite code
         }
-
         return h;
     }
 
+    /**
+     * Produces a string representation of the contents of the list.
+     * This exists for debugging purposes only.
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder("[");
-
-        for(SinglyLinkedList.Node walk = this.head; walk != null; walk = walk.getNext()) {
+        Node<E> walk = head;
+        while (walk != null) {
             sb.append(walk.getElement());
-            if (walk != this.tail) {
+            if (walk != tail)
                 sb.append(", ");
-            }
+            walk = walk.getNext();
         }
-
         sb.append("]");
         return sb.toString();
-    }
-
-    private static class Node<E> {
-        private E element;
-        private SinglyLinkedList.Node<E> next;
-
-        public Node(E e, SinglyLinkedList.Node<E> n) {
-            this.element = e;
-            this.next = n;
-        }
-
-        public E getElement() {
-            return this.element;
-        }
-
-        public SinglyLinkedList.Node<E> getNext() {
-            return this.next;
-        }
-
-        public void setNext(SinglyLinkedList.Node<E> n) {
-            this.next = n;
-        }
     }
 }
